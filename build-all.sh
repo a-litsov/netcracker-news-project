344d52e8a15188495e127d3f66a17fe6d2fc3679
alex <adlitsov@gmail.com>
2018-05-19 16:40:42 +0300

    Added comments service to build; now works without root rights

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cd articles-service; sudo mvn clean package; cd ..;
-cd config-server; sudo mvn clean package; cd ..;
-cd discovery-service; sudo mvn clean package; cd ..;
-cd gateway-service; sudo mvn clean package; cd ..;
+cd articles-service; mvn clean package; cd ..;
+cd comments-service; mvn clean package; cd ..;
+cd config-server; mvn clean package; cd ..;
+cd discovery-service; mvn clean package; cd ..;
+cd gateway-service; mvn clean package; cd ..;
