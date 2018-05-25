30ee0dfcd33e415c5d1cb0d9d963cb743517d1aa
alex <adlitsov@gmail.com>
2018-05-25 13:20:01 +0300

    Added side navigation; tried to make simple responsiveness (doesn't work for little sizes)

--- website/src/app/articles-dashb/articles-dashb.component.ts
+++ website/src/app/articles-dashb/articles-dashb.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { ArticlesService } from '../articles.service';
 import { Preview } from '../preview'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'articles-dashb',
@@ -9,24 +10,45 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./articles-dashb.component.css']
 })
 export class ArticlesDashbComponent {
-  previews: Preview[];
+  previews: Preview[] = [];
+  colsCount: number = 1;
+  cardMinWidth: number = 350;
 
-  constructor(private articlesService: ArticlesService, private route: ActivatedRoute) {
+  @ViewChild('gridContainer') gridContainer: ElementRef;
+
+  constructor(private articlesService: ArticlesService, private route: ActivatedRoute,
+              private cdRef:ChangeDetectorRef) {
   }
 
-  private loadPreview(id: number) {
+  private loadCategoryPreview(id: number) {
     this.articlesService.getPreviewByCategoryId(id).subscribe((inPreviews: Preview[]) => {
         this.previews = [...inPreviews];
         console.log(this.previews);
+        this.changeGridCols();
       },
       error => console.log("Error while obtaining all articles: ", error)
     );
   }
 
   ngOnInit() {
+    console.log("init");
     this.route.paramMap.subscribe((params: ParamMap) => {
       console.log(params.get('id'));
-      this.loadPreview(parseInt(params.get('id')));
+      this.loadCategoryPreview(parseInt(params.get('id')));
     });
+  }
+
+  changeGridCols() {
+    console.log("resize");
+    let gridWidth = this.gridContainer.nativeElement.clientWidth;
+    console.log(gridWidth);
+    this.colsCount = (gridWidth < this.cardMinWidth) ? 1 :
+      Math.min(this.previews.length, Math.floor(gridWidth / this.cardMinWidth));
+    this.cdRef.detectChanges();
+    console.log(this.colsCount);
+  }
+
+  onResize(event) {
+    this.changeGridCols();
   }
 }
--- website/src/app/main-nav/main-nav.component.ts
+++ website/src/app/main-nav/main-nav.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CategoriesService } from '../categories.service';
 import { Category } from '../category';
+import {MatSidenav} from "@angular/material";
+
 
 @Component({
   selector: 'main-nav',
@@ -13,6 +15,10 @@ import { Category } from '../category';
 export class MainNavComponent {
 
   categories: Category[];
+  isSideOpened: boolean = true;
+
+  @ViewChild('drawer') drawer;
+
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -35,4 +41,9 @@ export class MainNavComponent {
     this.getCategories();
   }
 
+  sideNavChanged() {
+    console.log("sidenav changed");
+    // Calling window.resize() to make dashboard resize
+    window.dispatchEvent(new Event('resize'));
+  }
 }
