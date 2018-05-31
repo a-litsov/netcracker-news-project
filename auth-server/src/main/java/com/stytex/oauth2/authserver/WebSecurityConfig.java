e4fe5bd309fb073a14286a3f0a890c0629281ace
alex <adlitsov@gmail.com>
2018-05-31 22:37:17 +0300

    auth-server: added simple auth server

---
+++ auth-server/src/main/java/com/stytex/oauth2/authserver/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.stytex.oauth2.authserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+            .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+            .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = passwordEncoder();
+        auth.inMemoryAuthentication()
+            .withUser("reader")
+                .password(encoder.encode("reader"))
+                .authorities("FOO_READ")
+            .and()
+            .withUser("writer")
+                .password(encoder.encode("writer"))
+                .authorities("FOO_READ", "FOO_WRITE");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
