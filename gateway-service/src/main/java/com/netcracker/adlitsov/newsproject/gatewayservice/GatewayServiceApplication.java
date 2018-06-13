ea53582a3eb43ef26d41f89cfd923032b55a10c2
alex <adlitsov@gmail.com>
2018-06-13 11:32:01 +0300

    gateway-service: added RelayTokenFilter (now can authenticate through it); added more routes

--- gateway-service/src/main/java/com/netcracker/adlitsov/newsproject/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/netcracker/adlitsov/newsproject/gatewayservice/GatewayServiceApplication.java
@@ -1,9 +1,11 @@
 package com.netcracker.adlitsov.newsproject.gatewayservice;
 
+import com.netcracker.adlitsov.newsproject.gatewayservice.filter.RelayTokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,5 +14,10 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RelayTokenFilter relayTokenFilter() {
+		return new RelayTokenFilter();
 	}
 }
